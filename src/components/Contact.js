e8b76830e4867f18dcee54e96f03d85e33641753
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2019-10-01 17:48:18 -0700

    Select

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import ContactInfo from "./ContactInfo";
+import ContactDetails from "./ContactDetails";
 
 export default class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectedKey: -1,
       keyword: "",
       contactData: [
         {
@@ -26,10 +28,18 @@ export default class Contact extends Component {
       ]
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(e) {
     this.setState({ keyword: e.target.value });
+  }
+
+  handleClick(key) {
+    this.setState({
+      selectedKey: key
+    });
+    console.log(key, "is seleted");
   }
 
   render() {
@@ -43,7 +53,15 @@ export default class Contact extends Component {
         return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
       });
       return data.map((contact, i) => {
-        return <ContactInfo contact={contact} key={i} />;
+        return (
+          <ContactInfo
+            contact={contact}
+            key={i}
+            onClick={() => {
+              this.handleClick(i);
+            }}
+          />
+        );
       });
     };
 
@@ -57,6 +75,10 @@ export default class Contact extends Component {
           onChange={this.handleChange}
         />
         <div>{mapToComponents(this.state.contactData)}</div>
+        <ContactDetails
+          isSelected={this.state.selectedKey !== -1}
+          contact={this.state.contactData[this.state.selectedKey]}
+        />
       </div>
     );
   }
