706ed53f94da3a28fb606151c2da8452a1e3882e
Dong kun Kim <dk@Dongs-MacBook-Air.local>
2019-10-01 22:45:52 -0700

    Key Press

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ContactInfo from "./ContactInfo";
 import ContactDetails from "./ContactDetails";
+import update from "immutability-helper";
+import ContactCreate from "./ContactCreate";
 
 export default class Contact extends Component {
   constructor(props) {
@@ -29,6 +31,9 @@ export default class Contact extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   handleChange(e) {
@@ -40,6 +45,35 @@ export default class Contact extends Component {
       selectedKey: key
     });
     console.log(key, "is seleted");
+  }
+
+  handleCreate(contact) {
+    this.setState({
+      contactData: update(this.state.contactData, { $push: [contact] })
+    });
+  }
+
+  handleRemove() {
+    if (this.state.selectedKey < 0) {
+      return;
+    }
+    this.setState({
+      contactData: update(this.state.contactData, {
+        $splice: [[this.state.selectedKey, 1]]
+      }),
+      selectedKey: -1
+    });
+  }
+
+  handleEdit(name, phone) {
+    this.setState({
+      contactData: update(this.state.contactData, {
+        [this.state.selectedKey]: {
+          name: { $set: name },
+          phone: { $set: phone }
+        }
+      })
+    });
   }
 
   render() {
@@ -78,7 +112,10 @@ export default class Contact extends Component {
         <ContactDetails
           isSelected={this.state.selectedKey !== -1}
           contact={this.state.contactData[this.state.selectedKey]}
+          onRemove={this.handleRemove}
+          onEdit={this.handleEdit}
         />
+        <ContactCreate onCreate={this.handleCreate} />
       </div>
     );
   }
